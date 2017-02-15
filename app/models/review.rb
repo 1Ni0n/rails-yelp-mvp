cc027bf99a95fa4ae1cc4662732aa47d5d990f6e
Anteo Guillot <anteo.guillot@gmail.com>
2017-02-15 15:39:56 +0100

    generated controllers for restaurants and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, :numericality => {:only_integer => true}
-
+  validates_format_of :rating, with: /\A[0-5]\z/
 end
