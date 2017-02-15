5f41e351e5a16a3a144151e97dedff25e7a11e04
Anteo Guillot <anteo.guillot@gmail.com>
2017-02-15 17:09:34 +0100

    seeded 5 restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'new/create'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only:[:index, :new, :create, :show] do
     resources :reviews, only:[ :new, :create ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+ {
+   name:         "Epicure au Bristol",
+   address:      "112 rue du Fg St-Honoré 75008 Paris",
+   phone_number: "06 01 02 03 04",
+   category:     "french"
+ },
+ {
+   name:         "La truffière",
+   address:      "4 rue Blainville 75005 Paris",
+   phone_number: "06 01 02 03 05",
+   category:     "chinese"
+ },
+ {
+   name:         "Le pré catelan",
+   address:      "route de Suresnes 75016 Paris",
+   phone_number: "06 01 02 03 06",
+   category:     "japanese"
+ },
+ {
+   name:         "Martine cuisine",
+   address:      "route de foulek 75016 Paris",
+   phone_number: "06 01 02 03 06",
+   category:     "italian"
+ },
+ {
+   name:         "L'amour est dans le pré",
+   address:      "route de Blaireaux 35206 Nantes",
+   phone_number: "06 01 02 03 06",
+   category:     "italian"
+ }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
